1b925ca2f68ee27afd0deda8ea9579661ba2fee0
Maria Kouman <maria_kuman@yahoo.de>
2017-07-09 23:01:31 +0200

    if exercise

--- 01-hello-world.py
+++ 01-hello-world.py
@@ -1,1 +1,9 @@
+# So zeigt man Text auf der Konsole
 print("This is an example line")
+# falls es eine if bedingung gibt
+x=1
+if x==1:
+    #indented four spaces
+    print("x is 1")
+
+print("Goodbye, World")
