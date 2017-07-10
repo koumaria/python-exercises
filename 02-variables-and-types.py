5d8c9d38268c0f986dcc0c0e5fe700e6463badb4
Maria Kouman <maria_kuman@yahoo.de>
2017-07-10 23:25:33 +0200

    string exercise

--- 02-variables-and-types.py
+++ 02-variables-and-types.py
@@ -4,8 +4,15 @@ print(myint)
 # floating point number
 myfloat = 7.0
 print(myfloat)
-#auch
+# auch
 myfloat = float(7)
 print(myfloat)
 
-
+# string
+mystring = 'hello'
+print(mystring)
+mystring = "hello"
+print(mystring)
+#string mit" unbedingt
+mystring="Don't worry about apostrophes"
+print(mystring)
