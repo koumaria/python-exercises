d8b78f0531aefdd51d8acba8a72738e95eda846d
Maria Kouman <maria_kuman@yahoo.de>
2017-07-12 22:42:12 +0200

    Exercise Mixing operators between numbers and strings will not work

--- 02-variables-and-types.py
+++ 02-variables-and-types.py
@@ -4,7 +4,7 @@ print(myint)
 # floating point number
 myfloat = 7.0
 print(myfloat)
-# auch
+# auch floating point number
 myfloat = float(7)
 print(myfloat)
 
@@ -13,6 +13,28 @@ mystring = 'hello'
 print(mystring)
 mystring = "hello"
 print(mystring)
-#string mit" unbedingt
-mystring="Don't worry about apostrophes"
+# wann string mit" unbedingt
+mystring = "Don't worry about apostrophes"
 print(mystring)
+
+# examples
+one = 1
+two = 2
+three = one + two
+print(three)
+
+hello = "bye"
+world = "world"
+helloworld = hello + "huhu" + world
+print(helloworld)
+
+a, b = 3, 4
+print(a, b)
+
+one = 1
+two = 2
+hello = "hello"
+# This will not work
+# print(one + two + hello)
+
+
